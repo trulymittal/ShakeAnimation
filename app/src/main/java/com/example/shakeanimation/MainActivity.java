b828d6fe84eea8ef09c28cbf4752da5850411f53
Truly Mittal <trulymittal@gmail.com>
2019-08-19 14:43:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shakeanimation/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.shakeanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        Animation shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake_animation);
+        button.setAnimation(shakeAnimation);
+    }
+}
